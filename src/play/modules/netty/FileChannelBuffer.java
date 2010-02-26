c3047b8addd788b84f3c49125b6f191706d16d05
Nicolas Leroux <nicolas@lunatech.com>
2010-02-26 14:53:11 +0100

    Stream files

---
+++ src/play/modules/netty/FileChannelBuffer.java
@@ -0,0 +1,325 @@
+/**
+ *
+ * Copyright 2010, Lunatech Labs.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ * User: nicolas
+ * Date: Feb 25, 2010
+ *
+ */
+package play.modules.netty;
+
+
+import org.jboss.netty.buffer.*;
+import play.Logger;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.GatheringByteChannel;
+import java.nio.channels.ScatteringByteChannel;
+
+
+public class FileChannelBuffer extends AbstractChannelBuffer implements WrappedChannelBuffer {
+
+    private final File file;
+    private final FileInputStream is;
+
+
+    public FileChannelBuffer(File file) {
+        if (file == null) {
+            throw new NullPointerException("file");
+        }
+        try {
+            this.file = file;
+            this.is = new TemporyFileInputStream(file);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public InputStream getInputStream() throws IOException {
+        return is;
+    }
+
+    public ChannelBuffer unwrap() {
+        throw new RuntimeException();
+    }
+
+    public ChannelBufferFactory factory() {
+        throw new RuntimeException();
+    }
+
+    public ByteOrder order() {
+        throw new RuntimeException();
+    }
+
+    public boolean isDirect() {
+        return true;
+    }
+
+    public boolean hasArray() {
+        return false;
+    }
+
+    public byte[] array() {
+        throw new RuntimeException();
+    }
+
+    public int arrayOffset() {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public void discardReadBytes() {
+        throw new RuntimeException();
+    }
+
+    public void setByte(int index, byte value) {
+        throw new RuntimeException();
+    }
+
+    public void setBytes(int index, ChannelBuffer src, int srcIndex, int length) {
+        throw new RuntimeException();
+    }
+
+    public void setBytes(int index, byte[] src, int srcIndex, int length) {
+        throw new RuntimeException();
+    }
+
+    public void setBytes(int index, ByteBuffer src) {
+        throw new RuntimeException();
+    }
+
+    public void setShort(int index, short value) {
+        throw new RuntimeException();
+    }
+
+    public void setMedium(int index, int value) {
+        throw new RuntimeException();
+    }
+
+    public void setInt(int index, int value) {
+        throw new RuntimeException();
+    }
+
+    public void setLong(int index, long value) {
+        throw new RuntimeException();
+    }
+
+    public int setBytes(int index, InputStream in, int length)
+            throws IOException {
+        Logger.info("RuntimeException");
+        throw new RuntimeException();
+    }
+
+    public int setBytes(int index, ScatteringByteChannel in, int length)
+            throws IOException {
+        Logger.info("RuntimeException");
+        throw new RuntimeException();
+
+    }
+
+    public int readerIndex() {
+        return 0;
+    }
+
+
+
+    public int getBytes(int index, GatheringByteChannel out, int length)
+            throws IOException {
+        Logger.info("getBytes");
+        byte[] b = new byte[length];
+        is.read(b, index, length);
+        ByteBuffer bb = ByteBuffer.wrap(b);
+        return out.write(bb);
+    }
+
+    public void getBytes(int index, OutputStream out, int length)
+            throws IOException {
+        Logger.info("getBytes");
+        byte[] b = new byte[length];
+        is.read(b, index, length);
+        out.write(b, index, length);
+    }
+
+    public void getBytes(int index, byte[] dst, int dstIndex, int length) {
+        try {
+            Logger.info("getBytes");
+            byte[] b = new byte[length];
+            is.read(b, index, length);
+            System.arraycopy(b, 0, dst, dstIndex, length);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void getBytes(int index, ChannelBuffer dst, int dstIndex, int length) {
+        try {
+            Logger.info("getBytes");
+            byte[] b = new byte[length];
+            is.read(b, index, length);
+            dst.writeBytes(b, dstIndex, length);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void getBytes(int index, ByteBuffer dst) {
+        try {
+            Logger.info("getBytes");
+            byte[] b = new byte[is.available() - index];
+            is.read(b, index, is.available() - index);
+            dst.put(b);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ChannelBuffer duplicate() {
+        Logger.info("duplicate");
+        throw new RuntimeException();
+    }
+
+    public ChannelBuffer copy(int index, int length) {
+        Logger.info("copy");
+        throw new RuntimeException();
+    }
+
+    public ChannelBuffer slice(int index, int length) {
+        Logger.info("slice");
+        throw new RuntimeException();
+    }
+
+    public byte getByte(int index) {
+        try {
+            byte[] b = new byte[1];
+            is.read(b, index, 1);
+            return b[0];
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public short getShort(int index) {
+        throw new RuntimeException();
+    }
+
+    public int getUnsignedMedium(int index) {
+        throw new RuntimeException();
+
+    }
+
+    public int getInt(int index) {
+        throw new RuntimeException();
+
+    }
+
+    public long getLong(int index) {
+        throw new RuntimeException();
+
+    }
+
+    public ByteBuffer toByteBuffer(int index, int length) {
+        //return buffer.getChannel().retoByteBuffer(index, length).asReadOnlyBuffer();
+        Logger.info("RuntimeException");
+        throw new RuntimeException();
+    }
+
+    @Override
+    public ByteBuffer[] toByteBuffers(int index, int length) {
+//        ByteBuffer[] bufs = buffer.toByteBuffers(index, length);
+//        for (int i = 0; i < bufs.length; i++) {
+//            bufs[i] = bufs[i].asReadOnlyBuffer();
+//        }
+//        return bufs;
+        Logger.info("RuntimeException");
+        throw new RuntimeException();
+    }
+
+    public int capacity() {
+        try {
+            return is.available();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public ChannelBuffer readBytes(int length) {
+          ChannelBuffer buf = ChannelBuffers.buffer(length);
+          getBytes(0, buf);
+          return buf;
+      }
+
+      public ChannelBuffer readBytes(ChannelBufferIndexFinder endIndexFinder) {
+          throw new RuntimeException();
+      }
+
+      public ChannelBuffer readSlice(int length) {
+          throw new RuntimeException();
+      }
+
+      public ChannelBuffer readSlice(ChannelBufferIndexFinder endIndexFinder) {
+         throw new RuntimeException();
+      }
+
+      public void readBytes(byte[] dst, int dstIndex, int length) {
+          checkReadableBytes(length);
+          getBytes(0, dst, dstIndex, length);
+      }
+
+      public void readBytes(byte[] dst) {
+          readBytes(dst, 0, dst.length);
+      }
+
+      public void readBytes(ChannelBuffer dst) {
+          readBytes(dst, dst.writableBytes());
+      }
+
+      public void readBytes(ChannelBuffer dst, int length) {
+          if (length > dst.writableBytes()) {
+              throw new IndexOutOfBoundsException();
+          }
+          readBytes(dst, dst.writerIndex(), length);
+          dst.writerIndex(dst.writerIndex() + length);
+      }
+
+      public void readBytes(ChannelBuffer dst, int dstIndex, int length) {
+          getBytes(0, dst, dstIndex, length);
+      }
+
+      public void readBytes(ByteBuffer dst) {
+          int length = dst.remaining();
+          checkReadableBytes(length);
+          getBytes(0, dst);
+      }
+
+      public int readBytes(GatheringByteChannel out, int length)
+              throws IOException {
+          checkReadableBytes(length);
+          int readBytes = getBytes(0, out, length);
+          return readBytes;
+      }
+
+      public void readBytes(OutputStream out, int length) throws IOException {
+          checkReadableBytes(length);
+          getBytes(0, out, length);
+      }
+
+}
---
+++ src/play/modules/netty/StreamChunkAggregator.java
@@ -0,0 +1,113 @@
+/**
+ *
+ * Copyright 2010, Lunatech Labs.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ * User: nicolas
+ * Date: Feb 25, 2010
+ *
+ */
+package play.modules.netty;
+
+import org.apache.commons.io.IOUtils;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
+import org.jboss.netty.handler.codec.http.HttpChunk;
+import org.jboss.netty.handler.codec.http.HttpHeaders;
+import org.jboss.netty.handler.codec.http.HttpMessage;
+import play.Logger;
+import play.Play;
+
+import java.io.*;
+import java.util.List;
+import java.util.UUID;
+
+public class StreamChunkAggregator extends SimpleChannelUpstreamHandler {
+
+    private volatile HttpMessage currentMessage;
+    private volatile String name;
+
+    /**
+     * Creates a new instance.
+     */
+    public StreamChunkAggregator() {
+        super();
+    }
+
+    @Override
+    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e)
+            throws Exception {
+
+        Object msg = e.getMessage();
+        if (!(msg instanceof HttpMessage) && !(msg instanceof HttpChunk)) {
+            ctx.sendUpstream(e);
+            return;
+        }
+
+        String localFile = this.name;
+        HttpMessage currentMessage = this.currentMessage;
+        if (currentMessage == null) {
+            HttpMessage m = (HttpMessage) msg;
+            if (m.isChunked()) {
+                final String localName = UUID.randomUUID().toString();
+
+                // A chunked message - remove 'Transfer-Encoding' header,
+                // initialize the cumulative buffer, and wait for incoming chunks.
+                // TODO Add HttpMessage/HttpChunkTrailer.removeHeader(name, value)
+                List<String> encodings = m.getHeaders(HttpHeaders.Names.TRANSFER_ENCODING);
+                encodings.remove(HttpHeaders.Values.CHUNKED);
+                if (encodings.isEmpty()) {
+                    m.removeHeader(HttpHeaders.Names.TRANSFER_ENCODING);
+                }
+                this.currentMessage = m;
+                this.name = localName;
+
+            } else {
+                // Not a chunked message - pass through.
+                ctx.sendUpstream(e);
+            }
+        } else {
+            // Merge the received chunk into the content of the current message.
+            final HttpChunk chunk = (HttpChunk) msg;
+            final File file = new File(Play.tmpDir, name);
+            final FileWriter fstream = new FileWriter(file, true);
+            final BufferedWriter out = new BufferedWriter(fstream);
+            IOUtils.copy(new ByteArrayInputStream(chunk.getContent().array()), out);
+            out.flush();
+            out.close();
+            fstream.close();
+
+            if (chunk.isLast()) {
+                currentMessage.setHeader(
+                        HttpHeaders.Names.CONTENT_LENGTH,
+                        String.valueOf(localFile.length()));
+
+
+                currentMessage.setContent(new FileChannelBuffer(file));
+
+                this.name = null;
+                this.currentMessage = null;
+                Channels.fireMessageReceived(ctx, currentMessage, e.getRemoteAddress());
+            }
+        }
+
+    }
+}
+
