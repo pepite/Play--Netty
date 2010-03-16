68f7a64082934633b681f9202b70e59298a63ad7
Nicolas Leroux <nicolas@lunatech.com>
2010-03-16 20:45:28 +0100

    Sample app

---
+++ samples-and-tests/test/app/controllers/Application.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import org.apache.commons.io.FileUtils;
+import play.Play;
+import play.mvc.*;
+import play.Logger;
+import java.io.File;
+
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+    public static void upload(File file) throws Exception {
+         Logger.info("File is " + file.getAbsolutePath() + "]");
+        FileUtils.copyFile(file, new File(Play.getFile("data/"), file.getName()));
+        Logger.info("File size [" + file.length() + "]");    
+    }
+
+}
